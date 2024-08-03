a655f5529a1776119fc956c712e048a638c98ade
Alex Yuwen <ayuwen@osisoft.com>
2024-08-02 21:11:24 -0700

    .

---
+++ mover.js
@@ -0,0 +1,25 @@
+class Mover {
+    constructor(x, y, m = 10) {
+      this.mass = m;
+      this.position = createVector(x, y);
+      this.velocity = createVector(0, 0);
+      this.acceleration = createVector(0, 0);
+    }
+  
+    applyForce(force) {
+      let f = p5.Vector.div(force, this.mass);
+      this.acceleration.add(f);
+    }
+  
+    update() {
+      this.velocity.add(this.acceleration);
+      this.position.add(this.velocity);
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(2);
+        fill(255);
+        circle(this.x, this.y, 40);
+    }
+  }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+/*
+
+
+
+GLOBAL VARIABLES
+
+
+
+*/
+
+let canvas;
+let mover;
+let gravity;
+
+/*
+
+
+
+MAIN FUNCTIONS
+
+
+
+*/
+
 function setup() {
-  createCanvas(400, 400);
+  getAudioContext().suspend();
+  canvas = createCanvas(1000, 500);
+  frameRate(60);
+  canvas.mousePressed(startAudio);
+
+  mover = new Mover(400, 400);
+  gravity = createVector(0, 0.1);
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  mover.applyForce(gravity);
+  mover.show();
+  mover.update();
+}
+
+function startAudio() {
+  userStartAudio();
 }
